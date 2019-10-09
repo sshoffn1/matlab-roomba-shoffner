e4d0b75a4f3c395b5e32684e82f1151e9bc35bf1
nreeder2 <55893613+nreeder2@users.noreply.github.com>
2019-10-09 11:40:08 -0400

    Add files via upload

---
+++ BumpSensorsWithResponse.m
@@ -0,0 +1,44 @@
+% roomba moves in straight line, pushes objects out of path then continues
+% straight
+v = 0.1
+r.setDriveVelocity(v,v)
+while true
+    bumps=r.getBumpers
+        if bumps.front==1
+            r.stop
+            r.setLEDs('All',1)
+            sendmail('6154154898@txt.att.net','Obstacle Detected','Ouch')
+            r.moveDistance(-0.4)
+            r.turnAngle(90)
+            r.moveDistance(0.4)
+            r.turnAngle(-90)
+            r.moveDistance(0.8)
+            r.turnAngle(-90)
+            r.moveDistance(0.4)
+            r.turnAngle(90)
+            r.setLEDs
+            r.setDriveVelocity(v,v)
+        elseif bumps.right==1
+            r.stop
+            r.setLEDs('All',1)
+            sendmail('6154154898@txt.att.net','Obstacle Detected','Ow')
+            r.moveDistance(-0.2)
+            r.turnAngle(-20)
+            r.moveDistance(0.4)
+            r.moveDistance(-0.4)
+            r.turnAngle(20)
+            r.setLEDs
+            r.setDriveVelocity(v,v)
+        elseif bumps.left==1
+            r.stop
+            r.setLEDs('All',1)
+            sendmail('6154154898@txt.att.net','Obstacle Detected','Ooof')
+            r.moveDistance(-0.2)
+            r.turnAngle(20)
+            r.moveDistance(0.4)
+            r.moveDistance(-0.4)
+            r.turnAngle(-20)
+            r.setLEDs
+            r.setDriveVelocity(v,v)
+        end
+end
