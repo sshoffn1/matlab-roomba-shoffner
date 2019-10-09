1974fc1a38f1a801a5ef0899af97903a94dff0ff
nreeder2 <55893613+nreeder2@users.noreply.github.com>
2019-10-08 20:09:03 -0400

    Update myEmail.m

--- myEmail.m
+++ myEmail.m
@@ -3,11 +3,20 @@ setpref('Internet','SMTP_Server','smtp.gmail.com')
 setpref('Internet','E_mail','iroomba.s6.2@gmail.com')
 setpref('Internet','SMTP_Username','iroomba.s6.2@gmail.com')
 setpref('Internet','SMTP_Password','EF230roomba')
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
 
-sendmail('recipients','subject','message') %command template 
+%sendmail('recipients','subject','message') %command template 
 %for roomba project should have difference versions put in if-else
 %loops to send different messages when robot does different tasks
 %or gets different out comes.
 
 % EXAMPLE 
-%sendmail('nreeder2@vols.utk.edu','iRoomba','email is successful')
+sendmail('nreeder2@vols.utk.edu','iRoomba','email is successful')
+
+%% to send a text use same format but change email address to
+% 'phonenumber@txt.att.net'                AT&T
+% 'phonenumber@tmomail.net'                T-Mobile
+% 'phonenumber@messaging.sprintpcs.com'    Sprint
+% 'phonenumber@vtext.com'                  Verizon
+% 'phonenumber@vmobl.com'                  Virgin Mobile
